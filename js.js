66657230ed5a28ce041744763056b578e8ee05b9
Jason Stidd <jason@frametech.us>
2013-05-31 22:09:24 -0600

    core

--- js.js
+++ js.js
@@ -1,18 +1,27 @@
 jQuery(document).ready(function($) {
   // Code using $ as usual goes here.
 
+    
+    $('#multi-table td').click(function(){ 
+        value = $(this).text();
+        intValue = parseInt(value);
+        $('#show-answer').text(intValue);	
+        if (intValue == (left * right)) {
+            $('#div-result').text('Correct');    
+        } else {
+            $('#div-result').text('Wrong');
+        }
+    });
 
-
-  $('#multi-table td').click(function(){ 
-	value = $(this).text();
-	intValue = parseInt(value);
-	if (intValue == 20) {
-	  $('#show-answer').text(intValue);	
-  	} else {
-
-	  $('#show-answer').text('Not 20');	
-	}
-  });
-
-	
+    $('#next-button').click(function() {
+        left = (Math.random() * 12 | 0) + 1;
+        right = (Math.random() * 12 | 0) + 1;
+        $('#div-question').text(left + ' x ' + right);
+        $('#show-answer').text('');
+        $('#div-result').text('');
+    });
+    
+    left = (Math.random() * 12 | 0) + 1;
+    right = (Math.random() * 12 | 0) + 1;
+    $('#div-question').text(left + ' x ' + right);
 });
